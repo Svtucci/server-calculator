204b3c0cb11e290ba9613fa1401ddf3291bf79c4
Stephen Vertucci <steve.vertucci@gmail.com>
2023-03-03 22:08:05 -0500

    server running off port 5001

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.use(express.static('server/public'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on: ${PORT}`); 
+})
