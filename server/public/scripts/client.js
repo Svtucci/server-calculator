d61d21ecea1610028dd551bbe14eda68bf033bdd
Stephen Vertucci <steve.vertucci@gmail.com>
2023-03-03 23:11:14 -0500

    changed format of calc to a drop down menu and took that value to add, as buttons were messing up logic

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,37 @@
 console.log('Script is sourced');
 
- 
+
+
+//  function addOperator(event) {
+//     operator = event.target.value;
+//     return operator
+//  };
+//  function subOperator(event) {
+//     operator = event.target.value;
+//     return operator
+//  };
+//  function multiOperator(event) {
+//     operator = event.target.value;
+//     return operator
+//  };
+//  function divOperator(event) {
+//     operator = event.target.value;
+//     return operator
+//  };
+
+
+
+
 
 function calculate (event) {
     event.preventDefault();
     let firstNum = Number(document.querySelector('#firstNumber').value);
     let secondNum = Number(document.querySelector('#secondNumber').value);
     console.log(firstNum, secondNum);
-
+    
     let operator = document.getElementById('operator').value;
+
+    console.log(operator);
 
     if (operator === "add") {
         result = firstNum + secondNum;
@@ -19,5 +42,5 @@ function calculate (event) {
     } else if (operator === "div") {
         result = firstNum / secondNum;
     }
-    document.getElementById('result').innerHTML = result:
+    console.log(result);
 }; 
